fdb6b39f329f649f15d7a284f072812e1b999d1a
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-04-07 10:35:35 -0400

    Calendar test using react-tailwindcss-datepicker with dayjs.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import art1 from '../assets/art1.png'
-import art2 from '../assets/art2.png'
-import art3 from '../assets/art3.png'
+import art1 from "../assets/art1.png";
+import art2 from "../assets/art2.png";
+import art3 from "../assets/art3.png";
 
 export default function Carousel() {
   return (
-    <div className="carousel rounded-box h- w-full">
-      <div className="carousel-item ">
-        <img src={art1} alt="sorted tattoos" />
+
+      <div className="carousel rounded-box min-h-1 p-4">
+        <div className="carousel-item ">
+          <img src={art1} alt="sorted tattoos" />
+        </div>
+        <div className="carousel-item">
+          <img src={art2} alt="sorted tattoos" />
+        </div>
+        <div className="carousel-item">
+          <img src={art3} alt="sorted tattoos" />
+        </div>
+        <div className="carousel-item">
+          <img src={art1} alt="sorted tattoos" />
+        </div>
+        <div className="carousel-item">
+          <img src={art2} alt="sorted tattoos" />
+        </div>
+        <div className="carousel-item">
+          <img src={art3} alt="sorted tattoos" />
+        </div>
       </div>
-      <div className="carousel-item">
-        <img src={art2} alt="sorted tattoos" />
-      </div>
-      <div className="carousel-item">
-        <img src={art3} alt="sorted tattoos" />
-      </div>
-      <div className="carousel-item">
-        <img src={art1} alt="sorted tattoos" />
-      </div>
-      <div className="carousel-item">
-        <img src={art2} alt="sorted tattoos" />
-      </div>
-      <div className="carousel-item">
-        <img src={art3} alt="sorted tattoos" />
-      </div>
-    </div>
+
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,31 @@
-import React from "react";
-import logo from '../assets/logo.png'
+import React, { useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+import logo from "../assets/logo.png";
 
 export default function Main() {
+  const [value, setValue] = useState({
+    startDate: new Date(),
+    endDate: new Date().setMonth(11),
+  });
+
+  const handleValueChange = (newValue) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <img
-          src={logo}
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
+        <img src={logo} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-5xl font-bold">FemTats</h1>
-          <p className="py-6">
-           
-          </p>
-          <button className="btn btn-primary">Saca Cita!</button>
+          <p className="py-6"></p>
+          <div className="chat chat-start">
+            <div className="chat-bubble chat-bubble-primary">
+              Saca Cita!
+            </div>
+          </div>
+          <Datepicker value={value} onChange={handleValueChange} />
         </div>
       </div>
     </div>
