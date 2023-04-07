f23c1c103dbd3e54bad0f5016c64f9b62be3c7f7
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-04-07 12:11:09 -0400

    Disabled Gallery.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Main from "./components/Main";
-import Carousel from "./components/Carousel";
 export default function App() {
   return (
     <div>
       <Main />
-      <Carousel />
     </div>
   );
 }
