f53871a79d24b596b963609398769416b7684763
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-04-07 11:49:26 -0400

    Change buttton that take you to IG.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
 
 export default function Main() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <img
-          src={logo}
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
+        <img src={logo} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 className="text-5xl font-bold">FemTats</h1>
-          <p className="py-6">
-           
-          </p>
-          <button className="btn btn-primary">Saca Cita!</button>
+          <h1 className="text-5xl font-bold"></h1>
+          <p className="py-6"></p>
+          <div className="chat chat-start">
+            <div className="chat-bubble chat-bubble-accent btn">
+             <p> <a href="https://www.instagram.com/fem.tats/">SACA CITA!</a></p>
+              
+            </div>
+          </div>
         </div>
       </div>
     </div>
