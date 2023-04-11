d31f0b7ed5255d74707385a59637a043a35db093
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-04-11 18:41:47 -0400

    Fixed gallery size.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Main from "./components/Main";
+import Carousel from "./components/Carousel";
 export default function App() {
   return (
     <div>
       <Main />
+      <Carousel />
     </div>
   );
 }
