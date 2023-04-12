787d81371a3176dc01587856681bb0a87fae95f7
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-04-11 22:04:33 -0400

    Testing media queries on gallery.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,20 +7,20 @@ export default function Carousel() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content">
-        <div className="carousel rounded-box">
-          <div className="carousel-item w-1/2">
+        <div className="w-96 carousel rounded-box">
+          <div className="carousel-item w-full md:w-1/2">
             <img
               src={art1}
               className="w-full"
             />
           </div>
-          <div className="carousel-item w-1/2">
+          <div className="carousel-item w-full md:w-1/2">
             <img
               src={art2}
               className="w-full"
             />
           </div>
-          <div className="carousel-item w-1/2">
+          <div className="carousel-item w-full md:w-1/2">
             <img
               src={art3}
               className="w-full"
