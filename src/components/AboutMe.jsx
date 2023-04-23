dcf519f9bca7259d9aac26c944c51e232ccadf33
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-04-22 21:07:46 -0400

    Another fix to text.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import alepinchando from "../assets/alepinchando.png";
 export default function AboutMe() {
   return (
-    <section id="aboutMe">
-      <div className="container flex flex-col justify-center p-5 mx-auto">
-        <div className="container flex justify-center mx-5">
-          <div className="flex my-auto text-center">
-            <p className="text-4xl font-bold text-center">
+    <section id="aboutme">
+      <div className="container p-5 mx-auto">
+        <div className=" grid md:grid-cols-2 justify-items-center">
+          <div className="grid m-5 p-5">
+            <p className="text-center text-2xl">
               Hola! Soy Alexandra, conocí el arte mediante el graffiti a mis 16
               años bajo el tag de FEM, y desde entonces me inspira la letra, las
               flores y las lineas gruesas y fluidas. El tatuaje me permite hacer
@@ -14,7 +14,9 @@ export default function AboutMe() {
               los procesos, y el stick and poke no es la excepción.
             </p>
           </div>
-          <img src={alepinchando} className="md:max-w-md" />
+          <div className="grid">
+            <img src={alepinchando} className="max-w-sm md:max-w-md" />
+          </div>
         </div>
       </div>
     </section>
